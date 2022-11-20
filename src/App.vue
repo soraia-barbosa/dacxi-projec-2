0cabeb82cb550fc6c6fb9ad0c0011574c55d9baa
Soraia Barbosa <so_raia_super@hotmail.com>
2022-11-19 23:44:03 -0300

    creating footer

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,18 @@
   
   <header>
     <div id=title>
-   <h1>Bitcoin today</h1>
+     <h1>Bitcoin today</h1>
   </div>
 </header>
-
-  <p>{{ current_price }}</p>
-  
-  <ul>
-    <li v-for="item in items" v-bind:key="item.id">
-      <p>Id: {{ item.id }}</p>
-      <p>Symbol: {{ item.symbol }}</p>
-    </li>
-  </ul>
+    <body>
+      <p>{{ current_price }}</p>
+       <img id=bitcoin src="../public/Bitcoin.png"/>
+</body>
+  <footer>
+    <div id=footer>
+       <h1>Bitcoin prices</h1>
+  </div>
+</footer>
 </template>
 
 <script>
@@ -25,49 +25,54 @@ export default {
   },
   data: () => ({
     current_price: 0,
-    items: [
-      {
-        id: "exemp",
-        symbol: "exemplo"
-      }
-    ]
+    
   }),
   mounted() {
     
     axios.get('https://api.coingecko.com/api/v3/coins/bitcoin').then((response) => {
       this.current_price = response.data.market_data.current_price.brl
     });
-    axios.get('https://api.coingecko.com/api/v3/coins/list').then((response) => {
-      this.items = response.data;
-      this.$forceUpdate();
-    });
+    
   }
 }
+
 </script>
 
 <style>
-
 *{
-
   margin: 0;
   padding: 0;
 }
-
 header{
-
   background-color: black;
   width: 100%;
   height: 60px;
-  
-  
 }
-
-#title {
-
+#title, #footer {
   color: white;
   padding: 10px;
-
  }
+
+ body{
+  min-height: 100%;
+ }
+
+ p {
+   margin-top: 70px;
+ }
+
+ #bitcoin {
+  width: 300px;
+  margin-top: 50px;
+  margin-bottom: 70px;
+ }
+footer{
+  background-color: black;
+  width: 100%;
+  height: 60px;
+  margin: auto;
+  bottom: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
